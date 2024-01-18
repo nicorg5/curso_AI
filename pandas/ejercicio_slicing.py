fb1d775ab0d558607635fe7c32f48f749c4a2d1d
n.c.rodriguez <n.c.rodriguez@amawfxfhngkyv.home>
2024-01-18 10:38:11 +0100

    Subida del ejercico de slicing

---
+++ pandas/ejercicio_slicing.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+# Datos para la serie de temperaturas semanales en grados Celsius
+serie_temperaturas = pd.Series([22, 24, 25, 21, 20, 23, 22])
+
+
+# Datos para la serie de precipitación semanal en milímetros
+serie_precipitacion= pd.Series([5, 10, 2, 15, 8, 3, 12])
+
+
+# Operaciones de slicing en ambas series
+#para obtener temperaturas de la tercera a la quinta semana
+sliced_serie_temperaturas = serie_temperaturas[2:6]
+
+#para obtener precipitaciiones de la tercera a la sexta semana
+sliced_serie_temperaturas = serie_precipitacion[2:7]
+
+
+# Combinación de las series resultantes del slicing en una nueva serie
+combined_serie = pd.concat([sliced_serie_temperaturas, sliced_serie_temperaturas], ignore_index=True)
+
+
+# Operaciones básicas en la Serie combinada
+#suma
+suma_combinada = combined_serie.sum()
+
+#promedio
+promedio_combinada = combined_serie.mean()
+
+
+# 4. Mostrar resultados
+print(f"Serie de temperaturas:\n{serie_temperaturas}\n")
+print(f"Serie de precipitaciones):\n{serie_precipitacion}\n")
+print(f"Slicing en serie de temperaturas (Semana 3 a Semana 5):\n{sliced_serie_temperaturas}\n")
+print(f"Slicing en serie de precipitaciones(Semana 3 a Semana 6):\n{sliced_serie_temperaturas}\n")
+print(f"Serie Combinada:\n{combined_serie}\n")
+print(f"Suma de la Serie Combinada: {suma_combinada}\n")
+print(f"Promedio de la Serie Combinada: {promedio_combinada}\n")
